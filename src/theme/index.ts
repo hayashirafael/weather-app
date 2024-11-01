0be04557dab8f0146d0f30a34bb29e55b901f76b
Rafael Hayashi <rafaelhayashi8@gmail.com>
2024-11-01 17:03:41 -0300

    feat: add app theme

---
+++ src/theme/index.ts
@@ -0,0 +1,14 @@
+export default {
+  COLORS: {
+    WHITE: '#FFFFFF',
+    GREY100: '#FAFAFA',
+    GREY400: '#7F7F98',
+    GREY500: '#3B3B54',
+    GREY600: '#22222F',
+    GREY900: '#13131A'
+  },
+  FONT_FAMILY: {
+    REGULAR: 'Nunito_400Regular',
+    BOLD: 'Nunito_700Bold'
+  }
+};
