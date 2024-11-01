6d803c1eebdfbfbabd2437a3666e16b5161305e6
Rafael Hayashi <rafaelhayashi8@gmail.com>
2024-11-01 17:02:20 -0300

    feat: add main file

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Routes } from '@routes/index';
+import { Loading } from '@components/Loading';
+import { WeatherContextProvider } from '@contexts/WeatherContext';
+import theme from '@theme/index';
+import { Nunito_400Regular, Nunito_700Bold, useFonts } from '@expo-google-fonts/nunito';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ThemeProvider } from 'styled-components/native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({ Nunito_400Regular, Nunito_700Bold });
+  if (!fontsLoaded) return <Loading />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <SafeAreaProvider style={{ flex: 1 }} >
+      <ThemeProvider theme={theme}>
+        <WeatherContextProvider>
+          <Routes />
+        </WeatherContextProvider>
+      </ThemeProvider>
+    </SafeAreaProvider>
+  );
+
+
+}
