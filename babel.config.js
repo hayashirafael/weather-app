4561b2f6b5a7012ac182fc6d8740a45afada7f69
Rafael Hayashi <rafaelhayashi8@gmail.com>
2024-11-01 16:59:17 -0300

    build: build files

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,26 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@dtos': './src/dtos',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@services': './src/services',
+            '@hooks': './src/hooks',
+            '@contexts': './src/contexts',
+            '@routes': './src/routes',
+            '@theme': './src/theme',
+            '@tests': './__tests__'
+          }
+        },
+      ],
+      ['module:react-native-dotenv']
+    ],
   };
 };
